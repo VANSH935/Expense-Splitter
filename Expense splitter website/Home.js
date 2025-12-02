3cbc2dbff38a494989b9caa12be03f7a1d7f0301
Durgesh05X <durgeshmakelkar05@gmail.com>
2025-12-02 12:54:07 +0530

    Update Home.js

--- Expense splitter website/Home.js
+++ Expense splitter website/Home.js
@@ -1,17 +1,15 @@
-// -------------------- AUTO SLIDE: HERO CAROUSEL --------------------
 const heroCarousel = document.querySelector('#heroCarousel');
 if (heroCarousel) {
   new bootstrap.Carousel(heroCarousel, {
-    interval: 3000, // 4 seconds
+    interval: 3000,
     ride: 'carousel'
   });
 }
 
-// -------------------- AUTO SLIDE: FEATURE CAROUSEL --------------------
 const featureCarousel = document.querySelector('#featureCarousel');
 if (featureCarousel) {
   new bootstrap.Carousel(featureCarousel, {
-    interval: 4000, // 7.5 seconds
+    interval: 3500,
     ride: 'carousel'
   });
 }
